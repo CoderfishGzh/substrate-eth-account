0070ecd2247c2223091d0db3011ed79058b9d38e
CoderfishGzh <335247945@qq.com>
2022-11-25 14:48:04 +0800

    finish eth account and verity

--- primitives/account/src/lib.rs
+++ primitives/account/src/lib.rs
@@ -3,13 +3,17 @@
 
 #![cfg_attr(not(feature = "std"), no_std)]
 
+use crate::{
+	ecdsa::{Public, Signature},
+	sp_runtime::traits::{IdentifyAccount, Lazy},
+};
 use codec::{Decode, Encode, MaxEncodedLen};
-use frame_support::sp_runtime::app_crypto::sp_core::{H160, ecdsa, RuntimeDebug};
+use frame_support::{
+	sp_io, sp_runtime,
+	sp_runtime::app_crypto::sp_core::{ecdsa, RuntimeDebug, H160, H256},
+};
 use scale_info::TypeInfo;
-use std::fmt::Formatter;
-use frame_support::sp_runtime;
-use crate::ecdsa::Signature;
-use crate::sp_runtime::traits::{IdentifyAccount, Lazy};
+use sha3::{Digest, Keccak256};
 
 /// Account 20 字节的帐号类型，与以太坊帐号类型保持一致
 #[derive(
@@ -19,14 +23,14 @@ pub struct AccountId20(pub [u8; 20]);
 
 #[cfg(feature = "std")]
 impl std::fmt::Display for AccountId20 {
-	fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+	fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
 		write!(f, "{:?}", self.0)
 	}
 }
 
 /// 赋予debug显示功能
 impl core::fmt::Debug for AccountId20 {
-	fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+	fn fmt(&self, f: &mut core::fmt::Formatter<'_>) -> core::fmt::Result {
 		write!(f, "{:?}", H160(self.0))
 	}
 }
@@ -83,13 +87,31 @@ impl From<ecdsa::Signature> for EthereumSignature {
 }
 
 /// 为 EthereumSignature 实现substrate 验证功能
-// impl sp_runtime::traits::Verify for EthereumSignature {
-// 	type Signer = EthereumSigner;
-//
-// 	fn verify<L: Lazy<[u8]>>(&self, msg: L, signer: &AccountId20) -> bool {
-//
-// 	}
-// }
+impl sp_runtime::traits::Verify for EthereumSignature {
+	type Signer = EthereumSigner;
+
+	fn verify<L: Lazy<[u8]>>(&self, mut msg: L, signer: &AccountId20) -> bool {
+		let mut m = [0u8; 32];
+		m.copy_from_slice(Keccak256::digest(msg.get()).as_slice());
+		match sp_io::crypto::secp256k1_ecdsa_recover(self.0.as_ref(), &m) {
+			Ok(pubkey) =>
+				AccountId20(H160::from(H256::from_slice(Keccak256::digest(&pubkey).as_slice())).0) ==
+					*signer,
+			Err(sp_io::EcdsaVerifyError::BadRS) => {
+				log::error!(target: "evm", "Error recovering: Incorrect value of R or S");
+				false
+			},
+			Err(sp_io::EcdsaVerifyError::BadV) => {
+				log::error!(target: "evm", "Error recovering: Incorrect value of V");
+				false
+			},
+			Err(sp_io::EcdsaVerifyError::BadSignature) => {
+				log::error!(target: "evm", "Error recovering: Invalid signature");
+				false
+			},
+		}
+	}
+}
 
 /// 以太坊和substrate 兼容的 公钥
 #[derive(Eq, PartialEq, Ord, PartialOrd, Clone, Encode, Decode, RuntimeDebug)]
@@ -101,5 +123,44 @@ impl sp_runtime::traits::IdentifyAccount for EthereumSigner {
 
 	fn into_account(self) -> Self::AccountId {
 		AccountId20(self.0)
+	}
+}
+
+impl From<[u8; 20]> for EthereumSigner {
+	fn from(value: [u8; 20]) -> Self {
+		EthereumSigner(value)
+	}
+}
+
+/// 将 ECDSA 压缩的 公钥 转化成 EthereumSigner
+impl From<ecdsa::Public> for EthereumSigner {
+	fn from(value: Public) -> Self {
+		let decompressed = libsecp256k1::PublicKey::parse_slice(
+			&value.0,
+			Some(libsecp256k1::PublicKeyFormat::Compressed),
+		)
+		.expect("Wrong compressed public key provide")
+		.serialize();
+		let mut m = [0u8; 64];
+		m.copy_from_slice(&decompressed[1..65]);
+		let account = H160::from(H256::from_slice(Keccak256::digest(&m).as_slice()));
+		EthereumSigner(account.into())
+	}
+}
+
+/// 将从 256k1曲线生成的公钥转换成EthereumSigner
+impl From<libsecp256k1::PublicKey> for EthereumSigner {
+	fn from(x: libsecp256k1::PublicKey) -> Self {
+		let mut m = [0u8; 64];
+		m.copy_from_slice(&x.serialize()[1..65]);
+		let account = H160::from(H256::from_slice(Keccak256::digest(&m).as_slice()));
+		EthereumSigner(account.into())
+	}
+}
+
+#[cfg(feature = "std")]
+impl std::fmt::Display for EthereumSigner {
+	fn fmt(&self, fmt: &mut std::fmt::Formatter) -> std::fmt::Result {
+		write!(fmt, "ethereum signature: {:?}", H160::from_slice(&self.0))
 	}
 }
