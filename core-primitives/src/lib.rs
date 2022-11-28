9dfb81629a2f745444d1d500aa9e75083392c8d7
CoderfishGzh <335247945@qq.com>
2022-11-28 15:51:20 +0800

    add core-primitives

--- core-primitives/src/lib.rs
+++ core-primitives/src/lib.rs
@@ -1,10 +1,27 @@
+// Copyright 2019-2022 PureStake Inc.
+// This file is part of Moonbeam.
+
+// Moonbeam is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+
+// Moonbeam is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+
+// You should have received a copy of the GNU General Public License
+// along with Moonbeam.  If not, see <http://www.gnu.org/licenses/>.
+
 #![cfg_attr(not(feature = "std"), no_std)]
 
 use account::EthereumSignature;
+use sp_runtime::traits::BlakeTwo256;
 pub use sp_runtime::OpaqueExtrinsic;
 use sp_runtime::{
-	generic,
-	traits::{BlakeTwo256, IdentifyAccount, Verify},
+    generic,
+    traits::{IdentifyAccount, Verify},
 };
 
 pub type Block = generic::Block<Header, OpaqueExtrinsic>;
